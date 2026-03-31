e44ee9c904a547d64dcbc81529cef7fcb727bf56
tawraaanshuu <tawra.anshu@gmail.com>
2026-03-31 23:09:54 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,20 @@
+window.SUPABASE_URL = 'https://yzmctktxzpzdfhdubwjs.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_BT1BJ5IKC7OArjTBdUBjkA_CjBZiFVf';
+
+// Common homepage IDs mapped to your real course slugs.
+window.COURSE_SLUG_MAP = {
+  'series-i': 'nism-series-i-currency-derivatives',
+  'nism-series-i': 'nism-series-i-currency-derivatives',
+  'currency-derivatives': 'nism-series-i-currency-derivatives',
+  'nism-series-i-currency-derivatives': 'nism-series-i-currency-derivatives',
+
+  'series-v-a': 'nism-series-v-a-mutual-fund-distributors',
+  'nism-series-v-a': 'nism-series-v-a-mutual-fund-distributors',
+  'mutual-fund-distributors': 'nism-series-v-a-mutual-fund-distributors',
+  'nism-series-v-a-mutual-fund-distributors': 'nism-series-v-a-mutual-fund-distributors',
+
+  'series-viii': 'nism-series-viii-equity-derivatives',
+  'nism-series-viii': 'nism-series-viii-equity-derivatives',
+  'equity-derivatives': 'nism-series-viii-equity-derivatives',
+  'nism-series-viii-equity-derivatives': 'nism-series-viii-equity-derivatives'
+};
