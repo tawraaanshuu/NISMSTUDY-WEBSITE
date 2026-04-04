45a55c92d2175b110edd07a5419a91405f3aed98
tawraaanshuu <tawra.anshu@gmail.com>
2026-04-05 00:53:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,11 @@ window.NISM_APP_CONFIG = {
   defaultPriceLabel: 'Rs 329 + GST',
   adminEmails: ['info@nismstudy.in'],
   tables: {
-    profiles: 'profiles',
-    courses: 'courses',
-    quizzes: 'quizzes',
-    examAccess: 'exam_access',
-    paymentRecords: 'payment_records',
-    mockAttempts: 'mock_attempts',
-    homeSupport: 'home_support_content'
-  }
+  profiles: 'profiles',
+  courses: 'courses',
+  quizzes: 'quizzes',
+  examAccess: 'enrollments',
+  paymentRecords: 'payments',
+  homeSupport: 'home_support_content'
+}
 };
