d5a4413de79ab732c0df50a3d9504100d6cc327b
tawraaanshuu <tawra.anshu@gmail.com>
2026-04-05 00:48:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.NISM_APP_CONFIG = {
   supabaseUrl: 'https://yzmctktxzpzdfhdubwjs.supabase.co',
   supabaseAnonKey: 'sb_publishable_BT1BJ5IKC7OArjTBdUBjkA_CjBZiFVf',
   accessDays: 15,
-  defaultPriceLabel: 'Rs 329',
-  adminEmails: ['tawra.anshu@gmail.com'],
+  defaultPriceLabel: 'Rs 329 + GST',
+  adminEmails: ['info@nismstudy.in'],
   tables: {
     profiles: 'profiles',
     courses: 'courses',
